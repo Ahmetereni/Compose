a22979593d776ba338a257b333316bcb21ba7083
Ahmetereni <94352384+Ahmetereni@users.noreply.github.com>
2023-02-22 17:31:14 +0300

    Add files via upload

---
+++ snackbar_Textfield/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.toast
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.toast.ui.theme.ToastTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember { mutableStateOf("") }
+            val scope= rememberCoroutineScope()
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            )
+            {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true, modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        scope.launch {                        scaffoldState.snackbarHostState.showSnackbar("hello$textFieldState")
+                        }
+                    })
+                    { Text(text = "click me!!")}
+                }
+            }
+
+
+        }
+    }
+}
+
